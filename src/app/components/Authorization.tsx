07f898ca9b4e2e831554c7d20ad1b5911affa620
anton <antonkuzmn1@gmail.com>
2025-02-16 21:01:08 +0700

    fix style

--- src/app/components/Authorization.tsx
+++ src/app/components/Authorization.tsx
@@ -76,7 +76,7 @@ const Authorization = () => {
                     </div>
                     <button
                         type="submit"
-                        className="w-full bg-white hover:bg-gray-300 border border-gray-300 text-black font-bold py-2 px-4 transition duration-300"
+                        className="w-full bg-white hover:bg-gray-300 border border-gray-300 text-black py-2 px-4 transition duration-300"
                     >
                         Submit
                     </button>
