451926ecfc448b3288c228b5411684ac36c57517
anton <antonkuzmn1@gmail.com>
2025-02-17 11:23:38 +0700

    restyle error/message dialog

--- src/app/components/ErrorMessage.tsx
+++ src/app/components/ErrorMessage.tsx
@@ -1,7 +1,7 @@
 import {AppDispatch, RootState} from "../../utils/store.ts";
 import {useDispatch, useSelector} from "react-redux";
 import {setAppError} from "../../slices/appSlice.ts";
-import {Close} from "@mui/icons-material";
+import Dialog from "./Dialog.tsx";
 
 const ErrorMessage = () => {
     const dispatch: AppDispatch = useDispatch();
@@ -11,29 +11,14 @@ const ErrorMessage = () => {
     if (error.length === 0) return null;
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-            <div className="border border-gray-300 bg-white max-w-md w-full">
-                <div className={'border-b h-8 border-gray-300 flex justify-between items-center'}>
-                    <h2 className={'h-full flex items-center justify-center px-2'}>
-                        Error
-                    </h2>
-                    <div
-                        className={'h-full flex items-center justify-center w-8 cursor-pointer hover:bg-gray-300 transition-colors duration-200'}
-                        onClick={() => dispatch(setAppError(''))}
-                    >
-                        <Close/>
-                    </div>
-                </div>
-                <div className="space-y-4 p-4">{error}</div>
-                <div className="border-t h-8 border-gray-300 flex justify-between items-center">
-                    <button
-                        className={'h-full w-full cursor-pointer hover:bg-gray-300 transition-colors duration-200'}
-                        onClick={() => dispatch(setAppError(''))}
-                        children={'Close'}
-                    />
-                </div>
-            </div>
-        </div>
+        <Dialog
+            title={"Error"}
+            close={() => dispatch(setAppError(""))}
+            message={error}
+            buttons={[
+                {text: "Close", onClick: () => dispatch(setAppError(""))},
+            ]}
+        />
     );
 };
 
--- src/app/components/Message.tsx
+++ src/app/components/Message.tsx
@@ -1,7 +1,7 @@
 import {AppDispatch, RootState} from "../../utils/store.ts";
 import {useDispatch, useSelector} from "react-redux";
 import {setAppMessage} from "../../slices/appSlice.ts";
-import {Close} from "@mui/icons-material";
+import Dialog from "./Dialog.tsx";
 
 const Message = () => {
     const dispatch: AppDispatch = useDispatch();
@@ -11,29 +11,14 @@ const Message = () => {
     if (message.length === 0) return null;
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-            <div className="border border-gray-300 bg-white max-w-md w-full">
-                <div className={'border-b h-8 border-gray-300 flex justify-between items-center'}>
-                    <h2 className={'h-full flex items-center justify-center px-2'}>
-                        Message
-                    </h2>
-                    <div
-                        className={'h-full flex items-center justify-center w-8 cursor-pointer hover:bg-gray-300 transition-colors duration-200'}
-                        onClick={() => dispatch(setAppMessage(''))}
-                    >
-                        <Close/>
-                    </div>
-                </div>
-                <div className="space-y-4 p-4">{message}</div>
-                <div className="border-t h-8 border-gray-300 flex justify-between items-center">
-                    <button
-                        className={'h-full w-full cursor-pointer hover:bg-gray-300 transition-colors duration-200'}
-                        onClick={() => dispatch(setAppMessage(''))}
-                        children={'Close'}
-                    />
-                </div>
-            </div>
-        </div>
+        <Dialog
+            title={"Message"}
+            close={() => dispatch(setAppMessage(""))}
+            message={message}
+            buttons={[
+                {text: "Close", onClick: () => dispatch(setAppMessage(""))},
+            ]}
+        />
     );
 };
 
